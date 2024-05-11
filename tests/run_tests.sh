a7b2f1e232afa7cfcf028d2a967881f10a24d903
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-05-11 09:50:14 -0700

    attempt local_save_dir fix for windows

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-pytest tests/krixik/utilities/
-pytest tests/krixik/modules/ 
-pytest tests/krixik/pipeline_builder/
-pytest tests/krixik/system_builder/ --dist=loadfile
-pytest tests/krixik/pipeline_examples/single_module/test_generate_examples.py
-pytest tests/krixik/pipeline_examples/multi_module/test_generate_examples.py
-pytest tests/krixik/pipeline_examples/single_module/test_coverage.py
-pytest tests/krixik/pipeline_examples/single_module/modules/ -n auto
-pytest tests/krixik/pipeline_examples/multi_module/test_multi_module_pipelines.py -n auto
+python3.10 -m pytest tests/krixik/utilities/
+python3.10 -m pytest tests/krixik/modules/ 
+python3.10 -m pytest tests/krixik/pipeline_builder/
+python3.10 -m pytest tests/krixik/system_builder/ --dist=loadfile
+python3.10 -m pytest tests/krixik/pipeline_examples/single_module/test_generate_examples.py
+python3.10 -m pytest tests/krixik/pipeline_examples/multi_module/test_generate_examples.py
+python3.10 -m pytest tests/krixik/pipeline_examples/single_module/test_coverage.py
+python3.10 -m pytest tests/krixik/pipeline_examples/single_module/modules/ -n auto
+python3.10 -m pytest tests/krixik/pipeline_examples/multi_module/test_multi_module_pipelines.py -n auto
